e1d7247ffbc869827b6048a22a44c865d84b3630
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-30 00:47:58 +0300

    fix

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const Danbooru = require('danbooru');
 
 const login = process.env.BOORU_LOGIN;
 const password = process.env.BOORU_KEY;
-async function getTiddies(){
-    const booru = new Danbooru(login + ":" + password);
-    const posts = await booru.posts({ tags: "solo areolae 1girl -loli" });
+const booru = new Danbooru(login + ":" + password);
+booru.posts({ tags: "solo areolae 1girl -loli" }).then((posts)=>{
     const index = Math.floor(Math.random() * posts.length);
     const post = posts[index];
     const url = booru.url(post.file_url);
-    return url;
-}
-console.log(getTiddies());
+    console.log(url);
+});
