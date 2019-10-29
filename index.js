5ba1bacc93df6133fcac60e7990dc2cadddc184c
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-10-30 00:38:09 +0300

    fixed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 'use strict';
-console.log('hello');
+const Danbooru = require('danbooru');
+
+const login = process.env.BOORU_LOGIN;
+const password = process.env.BOORU_KEY;
+
+const booru = new Danbooru(login + ":" + password);
+const posts = await booru.posts({ tags: "solo areolae 1girl -loli" });
+const index = Math.floor(Math.random() * posts.length);
+const post = posts[index];
+const url = booru.url(post.file_url);
+console.log(url);
