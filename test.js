5f9577d86e49e4459528a102ec575312796f4d8f
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-02 12:32:00 +0300

    test parameters

--- test.js
+++ test.js
@@ -1,12 +1,9 @@
 'use strict';
 let Danbooru;
 Danbooru = require('danbooru');
-const request = require('request-promise-native');
 
 const login = process.env.BOORU_LOGIN;
 const password = process.env.BOORU_KEY;
-const token = process.env.TG_TOKEN;
-const chatID = "@tiddies2d";
 
 async function getTiddies(){
     let tiddies = [];
@@ -16,6 +13,8 @@ async function getTiddies(){
         const page = await booru.posts({ limit: 200, page: i, tags: "solo breasts 1girl -loli score:>50" });
         posts.push(...page);
     }
+    console.log("array length:",posts.length);
+    console.log("first post format:", posts[0]);
     for (let i = 0; i < 5; i++) {
         const index = Math.floor(Math.random() * posts.length);
         const post = posts[index];
@@ -25,20 +24,5 @@ async function getTiddies(){
     return tiddies;
 }
 getTiddies().then(async (tiddies) => {
-    // let promise = new Promise((resolve, reject) => {
-    //     setTimeout(() => resolve("готово!"), 1000)
-    //   });
-    for (let i = 0; i < tiddies.length; i++) {
-        const element = tiddies[i];
-        try {
-            const url = "https://api.telegram.org/bot"+ token +
-            "/sendPhoto?photo=" + encodeURIComponent(element)+
-            "&chat_id=" + chatID;
-            console.log(url);
-            await request(url);
-//            await promise;
-        } catch (error) {
-            console.error(error);            
-        }
-    }
+  console.log(tiddies)
 });
