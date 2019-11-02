30de28775ea87df8c238c3c7dc5f7cb998257cec
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-02 15:18:00 +0300

    fixed original charachter

--- index.js
+++ index.js
@@ -60,11 +60,10 @@ getTiddies().then(async (tiddies) => {
                 "/sendPhoto?photo=" + encodeURIComponent(element.large_file_url)+
                 "&caption=" + encodeURIComponent("*Artist:* `" + element.tag_string_artist+"`\n"+
                                                 "*Origin:* `"+ element.tag_string_copyright.replace(/ /g,"\n")
-                                                        .replace(/_/g," ")+"`\n"+
+                                                        .replace(/_/g," ")+"`"+
                                                 ((element.tag_string_character !== "")?
-                                                    "*Character:* `"+ element.tag_string_character.replace(/ /g,"\n")
-                                                        .replace(/_/g," "):"")
-                                                +"`")+
+                                                    "\n*Character:* `"+ element.tag_string_character.replace(/ /g,"\n")
+                                                        .replace(/_/g," ")+"`":""))+
                 "&parse_mode=Markdown" +
                 "&chat_id=" + chatID;
             console.log(url);
