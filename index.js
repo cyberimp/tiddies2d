8ddd516d2dd274294138cbc2fa664b19dcb5f3de
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-02 13:21:04 +0300

    antibayan system

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 let Danbooru;
 Danbooru = require('danbooru');
 const request = require('request-promise-native');
+const { Client } = require('pg');
+
 
 const login = process.env.BOORU_LOGIN;
 const password = process.env.BOORU_KEY;
@@ -9,18 +11,42 @@ const token = process.env.TG_TOKEN;
 const chatID = "@tiddies2d";
 
 async function getTiddies(){
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
     let tiddies = [];
     const booru = new Danbooru(login + ":" + password);
+
+    client.connect();
+
     let posts =[];
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 6; i++) {
         const page = await booru.posts({ limit: 200, page: i, tags: "solo breasts 1girl -loli score:>50" });
         posts.push(...page);
     }
-    for (let i = 0; i < 5; i++) {
-        const index = Math.floor(Math.random() * posts.length);
-        const post = posts[index];
-        const url = booru.url(post.large_file_url);
-        tiddies.push(url.href);
+    try {
+        for (let i = 0; i < 5; i++) {
+            let post;
+            do {
+                const index = Math.floor(Math.random() * posts.length);
+                post = posts[index];
+            } while (async () => {
+                let query = "SELECT id FROM antibayan WHERE id =" + post.id + ";";
+                let res = await client.query(query);
+                return (res.rows.length < 1);
+            });
+//        const url = booru.url(post.large_file_url);
+            tiddies.push(post);
+            let query = "INSERT INTO antibayan(id,posted_at) VALUES(" + post.id + ",NOW());";
+            await client.query(query);
+        }
+    }
+    catch (error){
+        console.error(error);
+    }
+    finally {
+        client.end();
     }
     return tiddies;
 }
@@ -32,8 +58,9 @@ getTiddies().then(async (tiddies) => {
         const element = tiddies[i];
         try {
             const url = "https://api.telegram.org/bot"+ token +
-            "/sendPhoto?photo=" + encodeURIComponent(element)+
-            "&chat_id=" + chatID;
+                "/sendPhoto?photo=" + encodeURIComponent(element.large_file_url.href)+
+                "&caption=" + encodeURIComponent("Artist:" + element.tag_string_artist)+
+                "&chat_id=" + chatID;
             console.log(url);
             await request(url);
 //            await promise;
