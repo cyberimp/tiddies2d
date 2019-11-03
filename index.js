7fc8ad84bf7b08714ab3b018ea86bd4231a8118b
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-03 18:13:46 +0300

    test on index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let Danbooru;
 Danbooru = require('danbooru');
 const request = require('request-promise-native');
-const { Client } = require('pg');
+const {Client} = require('pg');
 
 
 const login = process.env.BOORU_LOGIN;
@@ -10,7 +10,7 @@ const password = process.env.BOORU_KEY;
 const token = process.env.TG_TOKEN;
 const chatID = "@tiddies2d";
 
-async function getTiddies(){
+async function getTiddies() {
     const client = new Client({
         connectionString: process.env.DATABASE_URL,
         ssl: true,
@@ -20,11 +20,26 @@ async function getTiddies(){
 
     client.connect();
 
-    let posts =[];
-    for (let i = 0; i < 6; i++) {
-        const page = await booru.posts({ limit: 200, page: i, tags: "solo breasts 1girl -loli score:>50" });
-        posts.push(...page);
+    let posts = [];
+    for (let j = 0; j < 2; j++) {
+        let promises = [];
+
+        for (let i = j * 20; i < (j + 1) * 20; i++) {
+            promises.push(booru.posts({limit: 200, page: i, tags: "solo breasts 1girl -loli score:>50"})
+                .then(result => {
+                    posts.push(...result);
+                }).catch(error => {
+                    console.error(error);
+                }));
+        }
+
+        for (let i = 0; i < promises.length; i++) {
+            await promises[i];
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
     }
+
     try {
         for (let i = 0; i < 5; i++) {
             let post;
@@ -34,21 +49,20 @@ async function getTiddies(){
                 post = posts[index];
                 let query = "SELECT id FROM antibayan WHERE id =" + post.id + ";";
                 res = await client.query(query);
-            } while (res.rows.length > 0 );
+            } while (res.rows.length > 0);
 //        const url = booru.url(post.large_file_url);
             tiddies.push(post);
             let query = "INSERT INTO antibayan(id,posted_at) VALUES(" + post.id + ",NOW());";
             await client.query(query);
         }
-    }
-    catch (error){
+    } catch (error) {
         console.error(error);
-    }
-    finally {
+    } finally {
         client.end();
     }
     return tiddies;
 }
+
 getTiddies().then(async (tiddies) => {
     // let promise = new Promise((resolve, reject) => {
     //     setTimeout(() => resolve("готово!"), 1000)
@@ -56,21 +70,25 @@ getTiddies().then(async (tiddies) => {
     for (let i = 0; i < tiddies.length; i++) {
         const element = tiddies[i];
         try {
-            const url = "https://api.telegram.org/bot"+ token +
-                "/sendPhoto?photo=" + encodeURIComponent(element.large_file_url)+
-                "&caption=" + encodeURIComponent("*Artist:* `" + element.tag_string_artist+"`\n"+
-                                                "*Origin:* `"+ element.tag_string_copyright.replace(/ /g,"\n")
-                                                        .replace(/_/g," ")+"`"+
-                                                ((element.tag_string_character !== "")?
-                                                    "\n*Character:* `"+ element.tag_string_character.replace(/ /g,"\n")
-                                                        .replace(/_/g," ")+"`":""))+
+            let extension = element.large_file_url.split(".").pop();
+            let command = "Photo?photo";
+            if (extension === "webm")
+                command = "Video?video";
+            const url = "https://api.telegram.org/bot" + token +
+                "/send" + command + "=" + encodeURIComponent(element.large_file_url) +
+                "&caption=" + encodeURIComponent("*Artist:* `" + element.tag_string_artist + "`\n" +
+                    "*Origin:* `" + element.tag_string_copyright.replace(/ /g, "\n")
+                        .replace(/_/g, " ") + "`" +
+                    ((element.tag_string_character !== "") ?
+                        "\n*Character:* `" + element.tag_string_character.replace(/ /g, "\n")
+                            .replace(/_/g, " ") + "`" : "")) +
                 "&parse_mode=Markdown" +
                 "&chat_id=" + chatID;
             console.log(url);
             await request(url);
 //            await promise;
         } catch (error) {
-            console.error(error);            
+            console.error(error);
         }
     }
 });
