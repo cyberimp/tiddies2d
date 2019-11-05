9f52bebc71015a55e7ce62084e188648829e1a78
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-05 20:33:55 +0300

    work pls

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ async function postTiddies(post) {
         "/send" + command + "?" + params;
     console.log(url);
     return request(url).then(res => {
-        if (res.ok === true) {
+        if (res.ok == "true") {
             addTiddies(post);
         }
         else {
