b78f95dae2bc27b712bfc3a1ff4f37d2ed79d313
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-05 19:47:37 +0300

    rewrote with cleaner functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Tiddies 2d
-Automatic moderator for adult anime channel
+Automatic moderator for adult anime channel.
+
+If you want me to help you with deployment of this application
+for your needs, donate me to https://qiwi.me/ovoshi
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let Danbooru;
 Danbooru = require('danbooru');
 const request = require('request-promise-native');
+const querystring = require('querystring');
 const {Client} = require('pg');
 
 
@@ -9,6 +10,15 @@ const login = process.env.BOORU_LOGIN;
 const password = process.env.BOORU_KEY;
 const token = process.env.TG_TOKEN;
 const chatID = "@tiddies2d";
+
+function containsObject(obj, list) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
 
 async function getTiddies() {
     const client = new Client({
@@ -50,11 +60,9 @@ async function getTiddies() {
                 post = posts[index];
                 let query = "SELECT id FROM antibayan WHERE id =" + post.id + ";";
                 res = await client.query(query);
-            } while (res.rows.length > 0);
+            } while (res.rows.length > 0 || containsObject(post,tiddies));
 //        const url = booru.url(post.large_file_url);
             tiddies.push(post);
-            let query = "INSERT INTO antibayan(id,posted_at) VALUES(" + post.id + ",NOW());";
-            await client.query(query);
         }
     } catch (error) {
         console.error(error);
@@ -64,31 +72,67 @@ async function getTiddies() {
     return tiddies;
 }
 
+function clearUnderscore(str) {
+    return str.replace(/ /g, "\n").replace(/_/g, " ")
+}
+
+async function addTiddies(post) {
+    const postID = post.id;
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: true,
+    });
+    await client.connect();
+    let query = "INSERT INTO antibayan(id,posted_at) VALUES($1,NOW());";
+    client.query(query, [postID]).then(result => {
+        console.log(result.rows[0]);
+    }, err => {
+        console.error(err)
+    }).finally(() => {
+        client.end();
+    });
+    return Promise.resolve();
+}
+
+function postTiddies(post) {
+    const postUrl = post.large_file_url;
+    const postArtist = post.tag_string_artist;
+    const postCopyright = post.tag_string_copyright;
+    const postCharacter = post.tag_string_character;
+    const extension = postUrl.split(".").pop();
+    let command = "Photo";
+    if (extension === "mp4" || extension === "gif")
+        command = "Video";
+
+    let paramsObj = {};
+    paramsObj[command.toLowerCase()] = postUrl;
+    paramsObj.caption = "*Artist:* `" + clearUnderscore(postArtist) + "`\n" +
+        "*Origin:* `" + clearUnderscore(postCopyright) + "`" +
+        ((postCharacter !== "") ?
+            "\n*Character:* `" + clearUnderscore(postCharacter) + "`"
+            : "");
+    paramsObj.parse_mode = "Markdown";
+    paramsObj.chat_id = chatID;
+    let params = querystring.stringify(paramsObj);
+    const url = "https://api.telegram.org/bot" + token +
+        "/send" + command + "?" + params;
+    console.log(url);
+    request(url).then(res => {
+        if (res.ok) {
+            addTiddies(post).then();
+        }
+    });
+}
+
 getTiddies().then(async (tiddies) => {
     for (let i = 0; i < tiddies.length; i++) {
         const element = tiddies[i];
         try {
-            let extension = element.large_file_url.split(".").pop();
-            let command = "Photo?photo";
-            if (extension === "mp4"|| extension === "gif")
-                command = "Video?video";
-            const url = "https://api.telegram.org/bot" + token +
-                "/send" + command + "=" + encodeURIComponent(element.large_file_url) +
-                "&caption=" + encodeURIComponent("*Artist:* `" + element.tag_string_artist + "`\n" +
-                    "*Origin:* `" + element.tag_string_copyright.replace(/ /g, "\n")
-                        .replace(/_/g, " ") + "`" +
-                    ((element.tag_string_character !== "") ?
-                        "\n*Character:* `" + element.tag_string_character.replace(/ /g, "\n")
-                            .replace(/_/g, " ") + "`" : "")) +
-                "&parse_mode=Markdown" +
-                "&chat_id=" + chatID;
-            console.log(url);
-            request(url).then(res => {
+            postTiddies(element).then(res => {
                 console.log(res)
             }, err => {
                 console.error(err)
             });
-//            await promise;
         } catch (error) {
             console.error(error);
         }
