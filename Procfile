764cd89aa1cf389b5d3fec12e46320b00a969ea9
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-17 16:33:55 +0300

    db compacting

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: node .
+dbcompact: node dbcompact.js
--- dbcompact.js
+++ dbcompact.js
@@ -9,9 +9,9 @@ client.query("SELECT * FROM antibayan ORDER BY posted_at")
         const total = res.rowCount;
         if (total > 5000){
             const lines = total - 4000;
-            let del = await client.query("DELETE FROM antibayan WHERE ctid IN (SELECT ctid FROM antibayan ORDER BY posted_at ASC LIMIT $1) ",[lines]);
+            let del = await client.query("DELETE FROM antibayan WHERE ctid IN (SELECT ctid FROM antibayan ORDER BY posted_at ASC LIMIT $1);",[lines]);
             console.log(del);
-            await client.query("VACUUM");
+            await client.query("VACUUM;");
             client.end();
         }
     });
