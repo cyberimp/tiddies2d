e4194d04c1b87ae9ec632f80b9cf1d8348bece15
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-17 16:10:48 +0300

    db compacting

--- dbcompact.js
+++ dbcompact.js
@@ -7,9 +7,9 @@ client.connect();
 client.query("SELECT * FROM antibayan ORDER BY posted_at")
     .then(async res => {
         const total = res.rowCount;
-        if (total > 1000){
-            const lines = total - 1000;
-            let del = await client.query("SELECT * FROM antibayan ORDER BY posted_at ASC LIMIT $1",[lines]);
+        if (total > 1128){
+            const lines = total - 1128;
+            let del = await client.query("WITH q AS (SELECT * FROM antibayan ORDER BY posted_at ASC LIMIT $1) DELETE FROM q",[lines]);
             console.log(del);
         }
     });
