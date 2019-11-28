cccaf050e2782b565784ba2916aaf7a7a6d07a68
Andrey Kalikin <kinzo.x.beato@gmail.com>
2019-11-28 17:27:50 +0300

    more results

--- dbcompact.js
+++ dbcompact.js
@@ -7,8 +7,8 @@ client.connect();
 client.query("SELECT * FROM antibayan ORDER BY posted_at")
     .then(async res => {
         const total = res.rowCount;
-        if (total > 5000){
-            const lines = total - 4000;
+        if (total > 3000){
+            const lines = total - 2000;
             await client.query("DELETE FROM antibayan WHERE ctid IN (SELECT ctid FROM antibayan ORDER BY posted_at ASC LIMIT $1);",[lines]);
             console.log(`deleted ${lines} first lines`);
             await client.query("VACUUM;");
